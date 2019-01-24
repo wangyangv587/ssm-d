25b9546472c461961ebec6e1b7c9d97e90fb8869
yangwangv5876 <yangwangv5876@163.com>
2019-01-24 23:45:19 +0800

    添加注释

--- src/main/java/com/kaikeba/dao/Config.java
+++ src/main/java/com/kaikeba/dao/Config.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(value = {"com.kaikeba.dao","com.kaikeba.aop"})
-@EnableAspectJAutoProxy(proxyTargetClass = false) //启用aspectj注解
+@EnableAspectJAutoProxy(proxyTargetClass = false) //启用aspectj注解，proxyTargetClass表示是否启用cglib的代理，默认为false，表示基于jdk的，那么应该基于接口，所以getBean的时候，参数只能为接口，否则报错
 public class Config {
 
 
